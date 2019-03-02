eb7d373a0ef2c9f9eddcf169b9d14d3a51f82918
Rupinderthind <rupinderpalthind@gmail.com>
2019-03-02 16:36:27 +0530

    Updates

---
+++ precache-manifest.d8a328eea78df2803f4a23a2f95d035d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6204df7b443512274ea3",
+    "url": "/side-menu-slim/static/js/runtime~main.6204df7b.js"
+  },
+  {
+    "revision": "b93d4b4b29da9cb34fd5",
+    "url": "/side-menu-slim/static/js/main.b93d4b4b.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/side-menu-slim/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "b93d4b4b29da9cb34fd5",
+    "url": "/side-menu-slim/static/css/main.3762fa63.chunk.css"
+  },
+  {
+    "revision": "c531d59ad5909f813ed93bda18571a69",
+    "url": "/side-menu-slim/index.html"
+  }
+];
